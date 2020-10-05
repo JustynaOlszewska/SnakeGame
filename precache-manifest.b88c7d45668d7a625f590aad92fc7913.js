0e9fa58d5d7208a2afe81a357c368c523e9237bd
Justyna Olszewka <wioza@wp.pl>
2020-10-05 11:27:43 +0200

    Updates

---
+++ precache-manifest.b88c7d45668d7a625f590aad92fc7913.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ce23b43e0849e59022f9209075db040",
+    "url": "/SnakeGame/index.html"
+  },
+  {
+    "revision": "8081657c3c375f1a89a5",
+    "url": "/SnakeGame/static/css/main.cbe46ef3.chunk.css"
+  },
+  {
+    "revision": "97e346cfd51cba4a8749",
+    "url": "/SnakeGame/static/js/2.d26445ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SnakeGame/static/js/2.d26445ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8081657c3c375f1a89a5",
+    "url": "/SnakeGame/static/js/main.350467e4.chunk.js"
+  },
+  {
+    "revision": "79138f707b70f767f171",
+    "url": "/SnakeGame/static/js/runtime-main.cfa2871d.js"
+  }
+]);
